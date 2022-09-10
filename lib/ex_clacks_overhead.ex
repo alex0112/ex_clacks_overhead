ed98dc4c56fd9ab8826798ecec7e347f3b3e1f52
alex0112 <alarsen0112@gmail.com>
2022-09-10 01:12:05 -0600

    init commit

---
+++ lib/ex_clacks_overhead.ex
@@ -0,0 +1,18 @@
+defmodule ExClacksOverhead do
+  @moduledoc """
+  Documentation for `ExClacksOverhead`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExClacksOverhead.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_clacks_overhead_test.exs
@@ -0,0 +1,8 @@
+defmodule ExClacksOverheadTest do
+  use ExUnit.Case
+  doctest ExClacksOverhead
+
+  test "greets the world" do
+    assert ExClacksOverhead.hello() == :world
+  end
+end
